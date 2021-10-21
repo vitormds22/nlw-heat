2c17ef8887558a1aa5f407122db457181ea35405
Vitor Fernandes <vitor.fernandes@masp.srv2>
2021-10-21 01:08:12 -0300

    Finalizando detalhes do frontend, posicionamento e imagem do user e começando a integração com o backend

--- nlw-web/src/components/MessageList/index.tsx
+++ nlw-web/src/components/MessageList/index.tsx
@@ -6,8 +6,46 @@ export function MessageList() {
         <div className={styles.messageListWrapper}>
             <img src={logo} alt="DoWhile 2021" />
 
-            <ul className={styles.messageListWrapper}>
-
+            <ul className={styles.messageList}>
+                <li className={styles.message}>
+                    <p className={styles.messageContent}>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti vero voluptates natus voluptate nemo at fugit, amet numquam ullam quo! Magnam corrupti doloribus eius, corporis amet facere at voluptate nobis.
+                    </p>
+                    <div className={styles.messageUser}>
+                        <div className={styles.userImage}>
+                            <img src="https://github.com/vitormds22.png" alt="Vitor Machado" />
+                        </div>
+                        <span>
+                            Vitor Machado
+                        </span>
+                    </div>
+                </li>
+                <li className={styles.message}>
+                    <p className={styles.messageContent}>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti vero voluptates natus voluptate nemo at fugit, amet numquam ullam quo! Magnam corrupti doloribus eius, corporis amet facere at voluptate nobis.
+                    </p>
+                    <div className={styles.messageUser}>
+                        <div className={styles.userImage}>
+                            <img src="https://github.com/vitormds22.png" alt="Vitor Machado" />
+                        </div>
+                        <span>
+                            Vitor Machado
+                        </span>
+                    </div>
+                </li>
+                <li className={styles.message}>
+                    <p className={styles.messageContent}>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti vero voluptates natus voluptate nemo at fugit, amet numquam ullam quo! Magnam corrupti doloribus eius, corporis amet facere at voluptate nobis.
+                    </p>
+                    <div className={styles.messageUser}>
+                        <div className={styles.userImage}>
+                            <img src="https://github.com/vitormds22.png" alt="Vitor Machado" />
+                        </div>
+                        <span>
+                            Vitor Machado
+                        </span>
+                    </div>
+                </li>
             </ul>
         </div>
     )
