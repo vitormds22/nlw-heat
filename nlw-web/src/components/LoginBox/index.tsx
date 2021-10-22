16374d975dc9ca67f5c4c7fb35125402d2c9fc93
Vitor Fernandes <vitor.fernandes@masp.srv2>
2021-10-22 00:40:35 -0300

    Criando o componente de formnulário da aplicação

--- nlw-web/src/components/LoginBox/index.tsx
+++ nlw-web/src/components/LoginBox/index.tsx
@@ -4,9 +4,7 @@ import { AuthContext } from '../../contexts/auth';
 import styles from './login.module.scss'
 
 export function LoginBox() {
-    const { signInUrl, user } = useContext(AuthContext)
-
-    console.log(user)
+    const { signInUrl } = useContext(AuthContext)
 
     return (
        <div className={styles.loginBoxWrapper}>
