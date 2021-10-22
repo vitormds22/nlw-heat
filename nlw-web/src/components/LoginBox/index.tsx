85b937d1a47053a065224165ecf4f89a355729b3
Vitor Fernandes <vitor.fernandes@masp.srv2>
2021-10-21 23:55:48 -0300

    Resolvendo o problema com o login e transferindo a lógica de validação para o repositorio contexts

--- nlw-web/src/components/LoginBox/index.tsx
+++ nlw-web/src/components/LoginBox/index.tsx
@@ -1,30 +1,12 @@
-import { useEffect, useRef } from 'react';
+import { useContext } from 'react';
 import { VscGithubInverted } from 'react-icons/vsc'
-import { api } from '../../services/api';
+import { AuthContext } from '../../contexts/auth';
 import styles from './login.module.scss'
 
-type AuthResponse = {
-    token: string,
-    user: {
-        id: string;
-        avatar_url: string;
-        name: string;
-        login: string;
-    }
-}
 export function LoginBox() {
-    const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=1d07a027b739f8fa312f`;
+    const { signInUrl, user } = useContext(AuthContext)
 
-    useEffect(() => {
-        const url = window.location.href;
-        const hasGithubCode = url.includes('?code=');
-
-        if(hasGithubCode) {
-            const [urlWithoutCode, githubCode] = url.split('?code=')
-
-            console.log({ urlWithoutCode, githubCode })
-        }
-    })
+    console.log(user)
 
     return (
        <div className={styles.loginBoxWrapper}>
